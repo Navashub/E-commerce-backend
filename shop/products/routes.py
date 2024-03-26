e9d093c1d5fbda950d1d1d87d7d11f6a6e97d2b1
Navashub <navasmuller01@gmail.com>
2024-03-26 14:08:14 +0300

    adding products to the database

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,6 +1,6 @@
 from flask import redirect, render_template, url_for, flash, request
-from shop import db, app
-from .models import Brand, Category
+from shop import db, app, photos
+from .models import Brand, Category, Addproduct
 from .forms import Addproducts
 from sqlalchemy.exc import IntegrityError
 
@@ -81,4 +81,23 @@ def addproduct():
     brands = Brand.query.all()
     categories = Category.query.all()
     form = Addproducts(request.form)
+    if request.method == 'POST':
+        name = form.name.data
+        price = form.price.data
+        discount = form.discount.data
+        stock = form.stock.data
+        colors = form.colors.data
+        desc = form.description.data
+        brand = request.form.get('brand')
+        category = request.form.get('category')
+        image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
+        image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
+
+        addpro = Addproduct(name=name, price=price, discount=discount, stock=stock, colors=colors, desc=desc, brand_id=brand, category_id=category, image_1=image_1, image_2=image_2)
+
+        db.session.add(addpro)
+        flash(f'The product {name} has been added to your database', 'success')
+        db.session.commit()
+        return redirect(url_for('admin'))
+    
     return render_template('products/addproduct.html', title='Add product page', form=form, brands=brands, categories=categories)
