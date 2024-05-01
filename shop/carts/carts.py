86c7fe9f65beeb621d0e92c14d5bada95c46534d
Navashub <navasmuller01@gmail.com>
2024-05-01 10:20:16 +0300

    updating the cart

--- shop/carts/carts.py
+++ shop/carts/carts.py
@@ -25,7 +25,7 @@ def AddCart():
         colors = request.form.get('colors')
         product = Addproduct.query.filter_by(id=product_id).first()
         if product_id and quantity and colors and request.method == "POST":
-            DictItems = {product_id:{'name': product.name, 'price': product.price, 'discount': product.discount, 'color': colors, 'quantity': quantity, 'image': product.image_1}}
+            DictItems = {product_id:{'name': product.name, 'price': product.price, 'discount': product.discount, 'color': colors, 'quantity': quantity, 'image': product.image_1, 'colors':product.colors}}
 
             if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
@@ -60,3 +60,31 @@ def getCart():
         grandtotal = float("%.2f" % (1.06 * subtotal))
 
     return render_template('products/carts.html', tax=tax, grandtotal=grandtotal)
+
+
+# @app.route('/empty')
+# def empty_cart():
+#     try:
+#         session.clear()
+#         return redirect(url_for('home'))
+#     except Exception as e:
+#         print(e)
+
+@app.route('/updatecart/<int:code>', methods=['POST'])
+def updatecart(code):
+    if 'Shoppingcart' not in session and len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key, item in session['Shoppingcart'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color'] = color
+                    flash('Item is updated')
+                    return redirect(url_for('getCart'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
